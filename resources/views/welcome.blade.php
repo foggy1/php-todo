7b06c99dce15192f44574c9b60641ee55e1a2e8b
Austin Lanari <austin.lanari@gmail.com>
2016-11-07 15:44:46 -0500

    Reconfigure front page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,15 +14,14 @@
 
         <div class="content">
             <div class="title m-b-md">
-                Laravel
+                PHP Todo
             </div>
 
             <div class="links">
-                <a href="https://laravel.com/docs">Documentation</a>
-                <a href="https://laracasts.com">Laracasts</a>
-                <a href="https://laravel-news.com">News</a>
-                <a href="https://forge.laravel.com">Forge</a>
-                <a href="https://github.com/laravel/laravel">GitHub</a>
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+                <a href="https://github.com/foggy1/php-todo" target="_blank">Project Repo</a>
+                <a href="https://laravel.com/docs" target="_blank">Laravel Docs</a>
             </div>
         </div>
     </div>
