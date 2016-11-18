c4a897867f6f9b385ffd91c1b8a3362500deab00
Austin Lanari <austin.lanari@gmail.com>
2016-11-17 23:25:54 -0500

    List users routes as links on index page

--- resources/views/user/lists/index.blade.php
+++ resources/views/user/lists/index.blade.php
@@ -13,8 +13,17 @@
         <a href="user/{{ $user->id }}/lists/create">Make new list</a>
       </div>
     </div>
-    <div class="row">
-        {{ $lists }}
+    <dic class="list-group">
+        @foreach ($lists as $list)
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                  <a href="#" class="list-group-item">
+                  <span class="tag tag-default tag-pill float-xs-right"></span>
+                    {{ $list->title }}
+                  </a>
+            </div>
+        </div>
+        @endforeach
     </div>
 </div>
 @endsection
