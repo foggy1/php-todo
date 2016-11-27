cb166befe52783a8b9e7b8ac6ccded68360138f9
Austin Lanari <austin.lanari@gmail.com>
2016-11-27 17:16:55 -0500

    Move list folder; change button text

--- resources/views/list/tasks/create.blade.php
+++ resources/views/list/tasks/create.blade.php
@@ -27,7 +27,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Make List
+                                    Register
                                 </button>
                             </div>
                         </div>
