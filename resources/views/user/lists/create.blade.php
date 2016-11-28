fef3a766532bc48a1c7f01eaeda84e70d2c988be
Austin Lanari <austin.lanari@gmail.com>
2016-11-28 17:10:31 -0500

    Fix page titles

--- resources/views/user/lists/create.blade.php
+++ resources/views/user/lists/create.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
+                <div class="panel-heading">List Creation</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ route('user.lists.store', ['user' => $user]) }}">
                         {{ csrf_field() }}
