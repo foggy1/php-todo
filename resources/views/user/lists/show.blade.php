02bc6199283a2596da0f8993dc7cd466cc3b83e4
Austin Lanari <austin.lanari@gmail.com>
2016-11-29 16:08:58 -0500

    Tasks displaying with glyphicons, widened and centered

--- resources/views/user/lists/show.blade.php
+++ resources/views/user/lists/show.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <h1 class="h1">{{ $list->title }}</h1>
+                <h1 class="h1 text-center">{{ ucfirst($list->title) }}</h1>
         </div>
     </div>
     <div class="row">
@@ -13,22 +13,18 @@
         <a href={{ route('list.tasks.create', ['list' => $list]) }}>Make new task</a>
       </div>
     </div>
-    <div id={{ $list->id }} class="list-group todos">
+    <div id={{ $list->id }} class="outer-container list-group todos">
         @foreach ($tasks as $task)
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
               <div id={{ $task->id }} class="input-group input-group-lg checkbox">
-                <span class="input-group-addon">
-                <input type="checkbox" value="" @if ($task->status === 1) checked  @endif>
+                <span style="width:100%;" class="input-group-addon">
+                
+                <label @if ($task->status === 1) style="text-decoration: line-through;" @endif><input type="checkbox" value="" @if ($task->status === 1) checked  @endif>{{ $task->description }}</label>
                 </span>
-                <label @if ($task->status === 1) style="text-decoration: line-through;" @endif>{{ $task->description }}</label>
                 <span class="input-group-btn">
                   <button class="btn btn-danger" type="button">
                     <i class="glyphicon glyphicon-remove"></i>
                   </button>
                 </span>
-              </div>
-            </div>
         </div>
         @endforeach
     </div>
