16fdce481019eaea6602c73802affda56b5ae0c4
Austin Lanari <austin.lanari@gmail.com>
2016-12-04 14:49:56 -0500

    Remove link to creation

--- resources/views/user/lists/show.blade.php
+++ resources/views/user/lists/show.blade.php
@@ -8,11 +8,6 @@
                 <h1 class="h1 text-center">{{ ucfirst($list->title) }}</h1>
         </div>
     </div>
-    <div class="row">
-      <div class="col-md-8 col-md-offset-2">
-        <a href={{ route('list.tasks.create', ['list' => $list]) }}>Make new task</a>
-      </div>
-    </div>
     <div id="app">
       <todo-list :list="{{$list}}"></todo-list>
 
